9794f76d8ae653d2a434912eb36f4cc585f45048
Jessica.W <jessica.watson1@live.com>
2021-11-02 11:13:22 -0600

    added local module and fat arrow

---
+++ 02-guild-argv/server.js
@@ -0,0 +1,20 @@
+// Accept command line input
+const inputString = process.argv[2];
+const guild = require('./guild')
+// TODO: clean up our expression
+console.log(inputString)
+
+const matchingCharacter = guild.find(function(item) {
+  if (inputString === item.name) {
+    return true;
+  } else {
+    return false;
+  }
+})
+
+// Log the results
+if (matchingCharacter) {
+  console.log(`Found ${inputString}: \n`, matchingCharacter);
+} else {
+  console.log(`Couldn't find any guild members with ${inputString} `);
+}
